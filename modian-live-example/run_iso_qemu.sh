60ae4237974785db65b369b8dc8c4bb105b527cf
Elena Grandi <elena@truelite.it>
2021-01-05 12:03:52 +0100

    Fix the comment about how to get a serial console

--- modian-live-example/run_iso_qemu.sh
+++ modian-live-example/run_iso_qemu.sh
@@ -8,12 +8,13 @@ set -e
 
 ISO=${1:-"dest/modian-example.iso"}
 
-# If the kernel parameters aren't passed correctly, they can be added manually
-# at the bootloader menu by pressing [tab]
+# To add custom kernel parameters we should pass a kernel to qemu and add 
+# -append "boot=live components debug timezone=Europe/Rome ip=frommedia consoleblank=0 console=ttyS0 printk.devkmsg=on"
+# to keep the script generic, you can press [tab] at the bootloader menu and
+# simply add the following options manually:
+# console=ttyS0 printk.devkmsg=on
 qemu-system-x86_64 \
     -m 1G \
-    -hdc "$ISO" \
+    -cdrom "$ISO" \
     -serial stdio \
     -boot d
-    -append "boot=live components debug timezone=Europe/Rome ip=frommedia consoleblank=0 console=ttyS0 printk.devkmsg=on"
-
