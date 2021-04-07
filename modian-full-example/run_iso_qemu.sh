34f1c5c15e88dab1d953df6e0993f8fd56d49917
Elena Grandi <elena@truelite.it>
2021-04-07 16:14:46 +0200

    Add a second disk to the qemu machine for tests

--- modian-full-example/run_iso_qemu.sh
+++ modian-full-example/run_iso_qemu.sh
@@ -3,10 +3,18 @@
 # Run an iso inside qemu, changing its boot options to print on serial so that
 # qemu can redirect it to the console, for ease of debugging.
 
+# A disk image live_test.qcow2 is generated in case it does not already exist.
+
 # stop on error
 set -e
 
 ISO=${1:-"dest/modian-full-example.iso"}
+
+if [ ! -e live_test.qcow2 ] ; then
+    qemu-img create -f qcow2 live_test.qcow2 40G
+fi
+
+bsdtar xf "$ISO" live/vmlinuz live/initrd.img
 
 # To add custom kernel parameters we should pass a kernel to qemu and add 
 # -append "boot=live components debug timezone=Europe/Rome ip=frommedia consoleblank=0 console=ttyS0 printk.devkmsg=on"
@@ -17,4 +25,9 @@ qemu-system-x86_64 \
     -m 1G \
     -cdrom "$ISO" \
     -serial stdio \
-    -boot d
+    -boot d \
+    -kernel live/vmlinuz \
+    -initrd live/initrd.img \
+    -append "boot=live components debug timezone=Europe/Rome ip=frommedia systemd.unit=modian-install.target consoleblank=0 console=ttyS0 printk.devkmsg=on" \
+    -hda live_test.qcow2
+
