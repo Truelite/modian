6d74ea81b3215015f9a9df559bf39911cfdf19a1
Elena Grandi <elena@truelite.it>
2021-04-30 15:07:53 +0200

    Example project running bullseye

---
+++ modian-full-bullseye-example/README.rst
@@ -0,0 +1,6 @@
+==============================
+ modian-full-bullseye-example
+==============================
+
+This is an example project that uses ``modian-live-wrapper`` and the
+rest of modian to generate a customized image based on Debian bullseye.
---
+++ modian-full-bullseye-example/run_installed_qemu.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Run the installed image live_test.qcow2 inside qemu.
+
+# stop on error
+set -e
+
+# To add custom kernel parameters we should pass a kernel to qemu and add
+# -append "boot=live ip=frommedia persistence-path=/live-0.1/ persistence
+# live-media-path=live-0.1 consoleblank=0 console=ttyS0 printk.devkmsg=on" to
+# keep the script generic, you can press [tab] at the bootloader menu and
+# simply add the following options manually: console=ttyS0 printk.devkmsg=on
+echo "running qemu"
+qemu-system-x86_64 \
+    -m 1G \
+    -serial stdio \
+    -hda live_test.qcow2
