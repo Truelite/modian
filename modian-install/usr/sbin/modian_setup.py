c302674d889bd88e7b6e8d10e1453b526126ba06
Elena Grandi <elena@truelite.it>
2021-11-19 14:09:33 +0100

    Command.main wasn't significantly different from InstallCommand.main

--- modian-install/usr/sbin/modian_setup.py
+++ modian-install/usr/sbin/modian_setup.py
@@ -9,19 +9,9 @@ import modian_install
 log = logging.getLogger()
 
 
-class Command(modian_install.command.InstallCommand):
-    def main(self):
-        self.setup()
-
-        self.hardware = modian_install.hardware.Hardware()
-        self.system = modian_install.hardware.System(self.hardware)
-        self.system.detect()
-        self.print_detection_report()
-
-
 if __name__ == "__main__":
     try:
-        Command().main()
+        modian_install.command.InstallCommand().main()
     except modian_install.actions.ModianError as e:
         log.error("%s", e)
         sys.exit(1)
