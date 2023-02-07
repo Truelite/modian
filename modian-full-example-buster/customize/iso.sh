9dd0ad865987a527de4c58aa09d6296cefba7806
Elena  Grandi <elena@truelite.it>
2023-02-07 11:35:36 +0100

    Also copy just the last version of the kernel in the iso in the other examples.

--- modian-full-example-buster/customize/iso.sh
+++ modian-full-example-buster/customize/iso.sh
@@ -2,6 +2,8 @@
 
 set -ue
 
-cp $1/live/vmlinuz-* $1/live/vmlinuz
-cp $1/live/initrd.img-* $1/live/initrd.img
+VMLINUX=$(ls $1/live/vmlinuz-*|sort -V|tail -n1)
+INITRD=$(ls $1/live/initrd.img-*|sort -V|tail -n1)
+cp $VMLINUX $1/live/vmlinuz
+cp $INITRD $1/live/initrd.img
 (cd $1 && md5sum ./live/* > md5sum.txt)
